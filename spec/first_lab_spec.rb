081c47df04a2ad63a8b0ed10b620832deaa53b01
aviflombaum <avi@flombaum.com>
2015-10-05 16:10:46 -0400

    Added tests

---
+++ spec/first_lab_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'First Lab:' do
+  it 'you made a change' do
+    new_file_made = Dir["*"].size > 5
+    file_edited = !File.read("./edit-me.txt").empty?
+    expect((new_file_made || file_edited)).to be_truthy, "Make sure you have added a new file or edited edit-me.txt"
+  end
+end
